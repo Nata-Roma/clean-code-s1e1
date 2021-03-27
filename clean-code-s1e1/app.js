26bf782563e9c38883648afb18bf47b30aa8c6b3
Natalia Romashko <sharkellgm@gmail>
2021-03-27 09:25:33 +0200

    refactor: adjust CSS-formatting according to rule 3.4 in html-and-css.md

--- clean-code-s1e1/app.js
+++ clean-code-s1e1/app.js
@@ -18,9 +18,11 @@ var completedTasksHolder=document.getElementById("completed-tasks");//completed-
 var createNewTaskElement=function(taskString){
 
     var listItem=document.createElement("li");
+    listItem.classList.add("list__item");
 
     //input (checkbox)
     var checkBox=document.createElement("input");//checkbx
+    checkBox.classList.add("list__checkbox");
     //label
     var label=document.createElement("label");//label
     //input (text)
@@ -33,18 +35,19 @@ var createNewTaskElement=function(taskString){
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className='task';
+    label.classList.add("list__label", 'task');
 
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.classList.add("list__input", "task");
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.classList.add("button", "edit");
 
-    deleteButton.className="delete";
+    deleteButton.classList.add("button", "delete");
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.classList.add("button__img");
     deleteButton.appendChild(deleteButtonImg);
 
 
